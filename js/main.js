3d0575b737943fa53d3fd835ccfe32c6bd48e819
Arafat Mohamed <arafatse@hotmail.com>
2017-09-17 01:29:50 +0200

    added videos with gif

--- js/main.js
+++ js/main.js
@@ -18,15 +18,17 @@ holoimg[0].onclick = function(){
 };
 window.onclick = function(e){
   if(e.target == modal) modal.style.display="none";
+  if(e.target == modal2) modal2.style.display="none";
+
 };
 
 function toLeft(){
-  if(i<900)i+=450;
+  if(i<1500)i+=450;
   modalContent.style.transform = "translate("+i+"px"+")";
 };
 
 function toRight(){
-  if(i>-900)i-=450;
+  if(i>-1500)i-=450;
   modalContent.style.transform = "translate("+i+"px"+")";
 };
 arrowRight.onmouseover = function(){toRight();};
@@ -52,4 +54,68 @@ fullModal.ondblclick = function(e){
   arrowRight.style.display = 'block';
   arrowLeft.style.display = 'block';
 };
+
+// fullModal.addEventListener('touchend', function(e) {
+//   for(var i=0;i<fullModal.childElementCount;i++){
+//     fullModal.children[i].style.display="none";
+//   };
+//   arrowRight.style.display = 'block';
+//   arrowLeft.style.display = 'block';
+// })
+
+
+
 //=================================================
+
+
+
+var
+  j = 0,
+  holoimg = document.getElementsByClassName("holoimg"),
+  modal2 = document.getElementsByClassName("modal")[1],
+  modalContent2 = document.getElementsByClassName("modal-content")[1],
+  fullModal2 = document.getElementsByClassName("full-modal")[1],
+  arrowLeft2 = document.getElementsByClassName("prev")[1],
+  arrowRight2 = document.getElementsByClassName("next")[1];
+// body.classList.add('loaded');
+body.onkeyup =function(e){
+  if(e.keyCode == 122 || e.keyCode == 27) body.classList.add('loaded');
+};
+
+holoimg[1].onclick = function(){
+  modal2.style.display="flex";
+  modalContent2.style.transform = "translate(0)"
+};
+
+function toLeft2(){
+  if(j<1500)j+=450;
+  modalContent2.style.transform = "translate("+j+"px"+")";
+};
+
+function toRight2(){
+  if(j>-1500)j-=450;
+  modalContent2.style.transform = "translate("+j+"px"+")";
+};
+arrowRight2.onmouseover = function(){toRight2();};
+arrowRight2.onclick = function(){toRight2();};
+arrowLeft2.onmouseover = function(){toLeft2();};
+arrowLeft2.onclick = function(){toLeft2();};
+modalContent2.onwheel = function(e){
+  if(e.deltaY > 0){toRight2()};
+  if(e.deltaY < 0){toLeft2()};
+};
+
+modalContent2.onclick = function(e){
+  var targetId = e.target.getAttribute("data-target");
+  document.getElementById(targetId).style.display = "block";
+  arrowRight2.style.display = 'none';
+  arrowLeft2.style.display = 'none';
+};
+
+fullModal2.ondblclick = function(e){
+  for(var i=0;i<fullModal2.childElementCount;i++){
+    fullModal2.children[i].style.display="none";
+  };
+  arrowRight2.style.display = 'block';
+  arrowLeft2.style.display = 'block';
+};
